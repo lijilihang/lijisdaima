ab7c00043e2c7f617b29292a87c77375d72ae612
lijilihang <1109925550@qq.com>
2018-05-07 13:15:27 +0800

    2018050713:15

--- graduation_design/src/com/etimeci/ssm/web/HelloController.java
+++ graduation_design/src/com/etimeci/ssm/web/HelloController.java
@@ -8,15 +8,13 @@ import com.etimeci.ssm.util.RedisCacheManager;
 
 @Controller
 public class HelloController {
+
     @Autowired
     private RedisCacheManager redisCacheManager;
-
 	@RequestMapping("/hello")
 	public String hello(){
-	    redisCacheManager.set("liji", "123");
+	    redisCacheManager.set("liji1", "dsdsssddsda");
 	    System.out.println(redisCacheManager.get("liji"));
 		return "success";
 	}
-
-
 }
